688bbf1c1ff594a1e359f1ad0b4ecf7584d0df5d
albertassi88 <albertassi88@gmail.com>
2021-10-14 20:15:25 -0300

    Alterando o MinBurgerSiderbar para pasta data

--- src/components/MinBurgerNavBar.js
+++ src/components/MinBurgerNavBar.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import * as FaIcons from 'react-icons/fa';
 import * as AiIcons from 'react-icons/ai';
 import { Link } from 'react-router-dom';
-import MinBurgerSiderBar from './MinBurgerSidebar';
+import MinBurgerSiderBar from '../data/MinBurgerSidebar';
 import { IconContext } from 'react-icons'; 
 import '../styles/MinBurgerNavBar.css'; 
 
