6e5bd9ff0f2fd4faaf65cb897b22a0c22838ea18
albertassi88 <albertassi88@gmail.com>
2021-10-14 23:45:07 -0300

    Alteracoes nas cores do FormSendEmail

--- src/components/MainHome.js
+++ src/components/MainHome.js
@@ -4,29 +4,29 @@ import photoHome from '../images/photoHome.jpg';
 import '../styles/MainHome.css';
 
 function MainHome() {
-    return (
-        <main>
-        <div data-aos="fade-down" className="box-main" id="home">
-          <img className="foto_home" src={photoHome} alt="minha foto"></img>
-          <h2>Olá, Eu sou
-            <br/><span className="home_title_color">Ruben Albertassi</span>
-            <br/>Desenvolvedor Web Full Stack
-          </h2>
-        </div>
-        <div className="box-main-button">
-          <a 
-            href="https://api.whatsapp.com/send?phone=5524988694508" 
-            target="_blank" 
-            rel="noreferrer"
-          >
-            <button type="button">
-              <SiWhatsapp />
-              <p>WhatsApp</p>
-            </button>
-          </a>
-        </div>
-      </main>
-    );
+  return (
+    <main>
+      <div data-aos="fade-down" className="box-main" id="home">
+        <img className="foto_home" src={photoHome} alt="minha foto"></img>
+        <h2>Olá, Eu sou
+          <br/><span className="home_title_color">Ruben Albertassi</span>
+          <br/>Desenvolvedor Web Full Stack
+        </h2>
+      </div>
+      <div className="box-main-button">
+        <a 
+          href="https://api.whatsapp.com/send?phone=5524988694508" 
+          target="_blank" 
+          rel="noreferrer"
+        >
+          <button type="button" data-aos="fade-down">
+            <SiWhatsapp />
+            <p>WhatsApp</p>
+          </button>
+        </a>
+      </div>
+    </main>
+  );
 }
 
 export default MainHome;
